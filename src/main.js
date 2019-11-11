10e605436720e975f5bca7031faf4d249c7effc2
hewo <jfdekrone@gmail.com>
2019-11-12 01:14:08 +0800

    added login form

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import jQuery from 'jquery'
+
+window.jQuery = jQuery;
+require('semantic-ui-css/semantic.css')
+require('semantic-ui-css/semantic.js')
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAZ6RxNbJXv6hQ3KdJnXU8T3kSqrXcVt0U',
+  authDomain: 'kredomerchandise@appspot.gserviceaccount.com',
+  projectId: 'kredomerchandise',
+  databaseURL: "https://kredomerchandise.firebaseio.com",
+});
+Vue.prototype.$db = firebase.firestore()
+
 
 Vue.config.productionTip = false
 
--- src/views/Auth/login.vue
+++ src/views/Auth/login.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-
+        <div class="ui cards" id="loginForm">
+            <div class="card">
+                <div class="content" style="padding: 20px 20px 20px 20px">
+                    <div class="header">
+                        <h3>Login</h3>
+                    </div>
+                    <form class="ui form">
+                        <div class="field">
+                            <label style="text-align: left;" for="">E-mail</label>
+                            <input type="text" placeholder="Enter E-mail">
+                        </div>
+                        <div class="field">
+                            <label style="text-align: left;" for="">Password</label>
+                            <input type="text" placeholder="Enter Password">
+                        </div>
+                        <div class="ui floating blue button">Login</div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +29,14 @@ export default {
 }
 </script>
 
-<style scoped lang="">
-    
+<style scoped>
+    #loginForm{
+        display: inline-block;
+        position: relative;
+        margin: 0 auto;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
 </style>
